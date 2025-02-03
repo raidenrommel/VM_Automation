9f93776a282f0c6b0129e934fbd3f52194d4f7ad
rommel <raidenrommel@gmail.com>
2025-02-03 10:57:16 +0800

    Initial commit - added automation scripts

--- main.tf
+++ main.tf
@@ -1,52 +1,52 @@
 provider "google" {
-  project     = "voltes-446205"
-  region      = "asia-southeast1"
-  credentials = file("/home/rommel/VmAutomation/voltes-446205-9de609428249.json")
+  project     = "<PROJECT_ID>"
+  region      = "<REGION>"
+  credentials = file("<CREDENTIALS_PATH>")
 }
 
 resource "google_compute_instance" "voltes_vm" {
-  name         = "voltes-vm"
-  machine_type = "e2-micro"
-  zone         = "asia-southeast1-a"
+  name         = "<VM_NAME>"
+  machine_type = "<MACHINE_TYPE>"
+  zone         = "<ZONE>"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "<IMAGE>"
     }
   }
 
   network_interface {
-    network = "default"
+    network = "<NETWORK>"
     access_config {}
   }
 
   service_account {
-    email  = "160842532634-compute@developer.gserviceaccount.com"
+    email  = "<SERVICE_ACCOUNT_EMAIL>"
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
 
 resource "google_storage_bucket" "functions_bucket" {
-  name     = "voltes-functions-bucket-446205"
-  location = "asia-southeast1"
+  name     = "<BUCKET_NAME>"
+  location = "<BUCKET_LOCATION>"
 }
 
 resource "google_storage_bucket_object" "stop_vm_zip" {
   name   = "stop_vm.zip"
   bucket = google_storage_bucket.functions_bucket.name
-  source = "/home/rommel/VmAutomation/stop_vm.zip"
+  source = "<STOP_VM_SOURCE_PATH>"
 }
 
 resource "google_storage_bucket_object" "start_vm_zip" {
   name   = "start_vm.zip"
   bucket = google_storage_bucket.functions_bucket.name
-  source = "/home/rommel/VmAutomation/start_vm.zip"
+  source = "<START_VM_SOURCE_PATH>"
 }
 
 resource "google_cloudfunctions_function" "stop_vm" {
   name        = "stop-vm-function"
   description = "Function to stop the VM"
-  runtime     = "python310"
+  runtime     = "<RUNTIME>"
 
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.functions_bucket.name
@@ -59,7 +59,7 @@ resource "google_cloudfunctions_function" "stop_vm" {
 resource "google_cloudfunctions_function" "start_vm" {
   name        = "start-vm-function"
   description = "Function to start the VM"
-  runtime     = "python310"
+  runtime     = "<RUNTIME>"
 
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.functions_bucket.name
@@ -71,15 +71,15 @@ resource "google_cloudfunctions_function" "start_vm" {
 
 resource "google_cloud_scheduler_job" "stop_vm" {
   name        = "stop-vm-job"
-  description = "Stop VM every Saturday at 4:00 PM "
+  description = "Stop VM every Saturday at 4:00 PM"
   schedule    = "0 16 * * 6"
-  time_zone   = "Asia/Manila"
+  time_zone   = "<TIME_ZONE>"
 
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.stop_vm.https_trigger_url
     oidc_token {
-      service_account_email = "160842532634-compute@developer.gserviceaccount.com"
+      service_account_email = "<SERVICE_ACCOUNT_EMAIL>"
     }
   }
 }
@@ -88,17 +88,16 @@ resource "google_cloud_scheduler_job" "start_vm" {
   name        = "start-vm-job"
   description = "Start VM every Sunday at 4:00 PM"
   schedule    = "0 16 * * 0"
-  time_zone   = "Asia/Manila"
+  time_zone   = "<TIME_ZONE>"
 
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.start_vm.https_trigger_url
     oidc_token {
-      service_account_email = "160842532634-compute@developer.gserviceaccount.com"
+      service_account_email = "<SERVICE_ACCOUNT_EMAIL>"
     }
   }
 }
-
 
 output "stop_vm_url" {
   value = google_cloudfunctions_function.stop_vm.https_trigger_url
@@ -107,3 +106,4 @@ output "stop_vm_url" {
 output "start_vm_url" {
   value = google_cloudfunctions_function.start_vm.https_trigger_url
 }
+
--- stop_vm.py
+++ stop_vm.py
@@ -4,8 +4,8 @@ from googleapiclient import discovery
 def stop_vm(request):
     credentials, project = google.auth.default()
     service = discovery.build('compute', 'v1', credentials=credentials)
-    instance = 'voltes-vm'
-    zone = 'asia-southeast1-a'
+    instance = 'name-vm'
+    zone = 'zone'
     
     request = service.instances().stop(project=project, zone=zone, instance=instance)
     response = request.execute()
